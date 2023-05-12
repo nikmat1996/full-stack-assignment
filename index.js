b133203e76c7e2572bf898fe65ab179a24103433
nikmat1996 <nikhilmathew1996@gmail.com>
2023-05-12 15:33:12 +0530

    add basic routes

--- index.js
+++ index.js
@@ -40,35 +40,63 @@ app.post('/signup', function(req, res) {
 })
 
 app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  
+  const { email, password } = req.body;
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  if(!email || !password)
+    return res.status(400).send("Email and Password required.")
 
+  let user = USERS.find( user => user.email === email );
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+  if(!user)
+    return res.status(401).send("User not found")
+
+  if(user.password === password)
+    return res.status(200).send("skdjbaskdjbasjdbkas")
+  else
+    return res.status(401).send("Wrong Password")
 
 })
 
 app.get('/questions', function(req, res) {
 
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  res.status(200).json(QUESTIONS)
+  
 })
 
 app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+
+  const { UserId, QuestionId } = req.body;
+
+  if(!UserId || !QuestionId)
+    return res.status(400).send("need to know who you are and what you want")
+  
+  let hisSubmissions = SUBMISSION.filter( solution => solution.UserId === UserId && solution.QuestionId === QuestionId )
+
+  if(hisSubmissions)
+    res.status(200).json(hisSubmissions)
+  else
+    res.status(200).send("No data found")
+
 });
 
 
 app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  
+  const { UserId, QuestionId, solution } = req.body;
+
+  if(!UserId || !QuestionId || !solution)
+    return res.status(400).send("Answer not in")
+  
+  let isAccepted = Math.random() <= 0.5;
+
+  SUBMISSION.push({ UserId , QuestionId, solution, isAccepted });
+
+  if(isAccepted)
+    return res.status(200).send("Good Answer")
+  else
+    return res.status(200).send("Wrong")
+
 });
 
 // leaving as hard todos
